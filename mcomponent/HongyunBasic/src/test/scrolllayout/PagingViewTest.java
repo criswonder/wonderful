a5b515304774b336d3b0c11fd3f7f7c58cd1f09d
criswonder <andymao@qq.com>
2013-04-03 12:46:09 +0800

    let it run

--- mcomponent/HongyunBasic/src/test/scrolllayout/PagingViewTest.java
+++ mcomponent/HongyunBasic/src/test/scrolllayout/PagingViewTest.java
@@ -10,34 +10,39 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import cn.mucang.android.common.config.MucangConfig;
-import cn.mucang.android.common.ui.PagingView;
+//import cn.mucang.android.common.config.MucangConfig;
+//import cn.mucang.android.common.ui.PagingView;
 
 import com.example.hongyunbasic.R;
 
+/**
+ * 测试mucang库中的PagingView 控件
+ * @author Andy Mao
+ *
+ */
 public class PagingViewTest extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		MucangConfig.setApplication(getApplication());
-		List<View> viewList = new ArrayList<View>();
-		LinearLayout i1 = new LinearLayout(this);
-		LinearLayout i2= new LinearLayout(this);
-		ImageView image1 = new ImageView(this);
-		ImageView image2 = new ImageView(this);
-		image1.setImageDrawable( getResources().getDrawable(R.drawable.test1));
-		image2.setImageDrawable( getResources().getDrawable(R.drawable.test2));
-		viewList.add(image2);
-		viewList.add(image1);
-		
-		Drawable pageOn = getResources().getDrawable(R.drawable.bg_arrow);
-		Drawable pageOff = getResources().getDrawable(R.drawable.bg_arrow);
-		Drawable pageBackGround = getResources().getDrawable(R.drawable.back_s);
-		PagingView pagingView = new PagingView(this, viewList, 10, pageOn, pageOff, pageBackGround);
-		pagingView.setAllowExceedLeft(false);
-		LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-		
-		setContentView(pagingView, params);
+//		MucangConfig.setApplication(getApplication());
+//		List<View> viewList = new ArrayList<View>();
+//		LinearLayout i1 = new LinearLayout(this);
+//		LinearLayout i2= new LinearLayout(this);
+//		ImageView image1 = new ImageView(this);
+//		ImageView image2 = new ImageView(this);
+//		image1.setImageDrawable( getResources().getDrawable(R.drawable.test1));
+//		image2.setImageDrawable( getResources().getDrawable(R.drawable.test2));
+//		viewList.add(image2);
+//		viewList.add(image1);
+//		
+//		Drawable pageOn = getResources().getDrawable(R.drawable.bg_arrow);
+//		Drawable pageOff = getResources().getDrawable(R.drawable.bg_arrow);
+//		Drawable pageBackGround = getResources().getDrawable(R.drawable.back_s);
+//		PagingView pagingView = new PagingView(this, viewList, 10, pageOn, pageOff, pageBackGround);
+//		pagingView.setAllowExceedLeft(false);
+//		LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+//		
+//		setContentView(pagingView, params);
 	}
 
 }
