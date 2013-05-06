c2bf33939e6df86fd1bb580660b09dbca44d17bb
criswonder <andymao@qq.com>
2013-05-06 21:36:57 +0800

    test chexian number component

---
+++ mcomponent/HongyunBasic/src/com/hongyun/component/CheXianNumberView.java
@@ -0,0 +1,23 @@
+package com.hongyun.component;
+
+import android.content.Context;
+import android.view.View;
+
+public class CheXianNumberView extends View {
+
+	public CheXianNumberView(Context context) {
+		super(context);
+	}
+
+	public void loop() {
+
+	}
+
+	public void set(int cellCount, int duration, int maxDistance) {
+
+	}
+
+	public void setNumber(final int newNumber, final boolean animation) {
+	}
+
+}
