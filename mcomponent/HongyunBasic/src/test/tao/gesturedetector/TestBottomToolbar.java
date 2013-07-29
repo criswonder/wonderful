df497e516af1b1271a286dccf46b0e0cfba6c248
criswonder <andymao@qq.com>
2013-07-29 19:12:18 +0800

    add test/tao/gesturedetector/TestBottomToolbar.java
    Signed-off-by: criswonder <andymao@qq.com>

---
+++ mcomponent/HongyunBasic/src/test/tao/gesturedetector/TestBottomToolbar.java
@@ -0,0 +1,14 @@
+package test.tao.gesturedetector;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class TestBottomToolbar extends ListActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setListAdapter(new simple);
+	}
+
+	
+}
